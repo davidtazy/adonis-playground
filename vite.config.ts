5dd4204b353a613f4754a861e2a113c454c73b45
davidtazy <david.allemant@gmail.com>
2024-10-08 21:55:27 +0200

    2.6 Setting Up Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import adonisjs from '@adonisjs/vite/client'
 
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 export default defineConfig({
   plugins: [
     adonisjs({
@@ -16,4 +19,9 @@ export default defineConfig({
       reload: ['resources/views/**/*.edge'],
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
 })
