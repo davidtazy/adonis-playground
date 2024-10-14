479ba6251be1b2aca4feb1c13fb7f2c591dd426b
davidtazy <david.allemant@gmail.com>
2024-10-14 21:29:56 +0200

    4.12 Adding a profile model, migration,  factory and controller

---
+++ database/migrations/1728933569469_create_profiles_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'profiles'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').notNullable()
+
+      table.integer('user_id').unsigned().references('users.id').notNullable()
+      table.text('description')
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
