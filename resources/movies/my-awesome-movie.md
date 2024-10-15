a52cd25c4986c226bd62f767176ff035e715ac5e
davidtazy <david.allemant@gmail.com>
2024-10-15 23:58:07 +0200

    5.6 Listing A Director's Movies with Relationship Existence Queries

--- resources/movies/my-awesome-movie.md
+++
@@ -1,8 +0,0 @@
----
-title: My awesome movie
-summary: dolor sit amet, qui aute
----
-
-
-
-Lorem ipsum dolor sit amet, qui aute ex do do ea amet nulla ad consectetur
--- start/routes.ts
+++ start/routes.ts
@@ -7,9 +7,11 @@
 |
 */
 
-const RedisController = () => import('#controllers/redis_controller')
 import router from '@adonisjs/core/services/router'
+
+const RedisController = () => import('#controllers/redis_controller')
 const MoviesController = () => import('#controllers/movies_controller')
+const DirectorsController = () => import('#controllers/directors_controller')
 
 router.get('/', [MoviesController, 'index']).as('home')
 
@@ -17,6 +19,9 @@ router
   .get('/movies/:slug', [MoviesController, 'show'])
   .as('movies.show')
   .where('slug', router.matchers.slug())
+
+router.get('/directors/:id', [DirectorsController, 'show']).as('directors.show')
+router.get('/directors', [DirectorsController, 'index']).as('directors.index')
 
 // flush is first because it would be treated as a valid slug (route are tested in order)
 router.delete('/redis/flush', [RedisController, 'flush']).as('redis.flush')
