3419134925c7e4213c81389277d0ca75e0424b3a
davidtazy <david.allemant@gmail.com>
2024-10-27 22:39:29 +0100

    11.5 How To Use One Form to Create or Edit Movies

--- app/controllers/admin/movies_controller.ts
+++ app/controllers/admin/movies_controller.ts
@@ -29,7 +29,7 @@ export default class MoviesController {
    */
   async create({ view }: HttpContext) {
     const data = await MovieService.getFormData()
-    return view.render('pages/admin/movies/create', data)
+    return view.render('pages/admin/movies/createOrUpdate', data)
   }
 
   /**
@@ -52,7 +52,7 @@ export default class MoviesController {
   async edit({ params, view }: HttpContext) {
     const movie = await Movie.findOrFail(params.id)
     const data = await MovieService.getFormData()
-    return view.render('pages/admin/movies/edit', { movie: movie, ...data })
+    return view.render('pages/admin/movies/createOrUpdate', { movie: movie, ...data })
   }
 
   /**
