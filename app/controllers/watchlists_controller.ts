51e4efc0b9480af441f47cc9dd0e49495de15046
davidtazy <david.allemant@gmail.com>
2024-10-29 22:04:30 +0100

    9.4 Filtering By User's Watched Status

--- app/controllers/watchlists_controller.ts
+++ app/controllers/watchlists_controller.ts
@@ -1,7 +1,7 @@
 import MovieStatus from '#models/movie_status'
 import Watchlist from '#models/watchlist'
 import MovieService from '#services/movie_service'
-import { movieFilterValidator } from '#validators/movies_filter'
+import { watchlistFilterValidator } from '#validators/movies_filter'
 import type { HttpContext } from '@adonisjs/core/http'
 import router from '@adonisjs/core/services/router'
 import { DateTime } from 'luxon'
@@ -9,12 +9,17 @@ import querystring from 'node:querystring'
 
 export default class WatchlistsController {
   async index({ request, view, auth }: HttpContext) {
-    const filters = await movieFilterValidator.validate(request.qs())
+    const filters = await watchlistFilterValidator.validate(request.qs())
 
     const page = request.input('page', 1) as number
 
     const movies = await MovieService.getFiltered(filters, auth.user)
-      .whereHas('watchlist', (query) => query.where('userId', auth.user!.id))
+      .whereHas('watchlist', (query) =>
+        query
+          .where('userId', auth.user!.id)
+          .if(filters.watched === 'watched', (watchlist) => watchlist.whereNotNull('watchedAt'))
+          .if(filters.watched === 'unwatched', (watchlist) => watchlist.whereNull('watchedAt'))
+      )
       .paginate(page, 15)
 
     const movieStatuses = await MovieStatus.query().orderBy('name').select('id', 'name')
@@ -59,6 +64,6 @@ export default class WatchlistsController {
       watchlist.watchedAt = DateTime.now()
     }
     await watchlist.save()
-    return response.redirect().back()
+    return response.redirect().withQs().back()
   }
 }
--- app/validators/movies_filter.ts
+++ app/validators/movies_filter.ts
@@ -1,20 +1,27 @@
 import MovieService from '#services/movie_service'
 import vine from '@vinejs/vine'
 
-export const movieFilterValidator = vine.compile(
+const sharedMovieFilterSchema = vine.object({
+  search: vine
+    .string() /*.alphaNumeric({ allowSpaces: true })*/
+    .optional(),
+  status: vine
+    .number()
+    .isExists({ table: 'movie_statuses', column: 'id', accept_all_if_zero: true })
+    .optional(),
+  sort: vine
+    .string()
+    .exists(async (_db, value) => {
+      return MovieService.sortOptions.some((option) => option.id === value)
+    })
+    .optional(),
+})
+
+export const movieFilterValidator = vine.compile(sharedMovieFilterSchema)
+
+export const watchlistFilterValidator = vine.compile(
   vine.object({
-    search: vine
-      .string() /*.alphaNumeric({ allowSpaces: true })*/
-      .optional(),
-    status: vine
-      .number()
-      .isExists({ table: 'movie_statuses', column: 'id', accept_all_if_zero: true })
-      .optional(),
-    sort: vine
-      .string()
-      .exists(async (_db, value) => {
-        return MovieService.sortOptions.some((option) => option.id === value)
-      })
-      .optional(),
+    ...sharedMovieFilterSchema.getProperties(),
+    watched: vine.enum(['all', 'watched', 'unwatched']).optional(),
   })
 )
