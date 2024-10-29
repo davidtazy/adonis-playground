19edabff5433c80342e365d3fc23600b77ce111f
davidtazy <david.allemant@gmail.com>
2024-10-30 00:25:00 +0100

    10.2 Saving All Or Nothing with Database Transactions

--- app/controllers/profiles_controller.ts
+++ app/controllers/profiles_controller.ts
@@ -2,6 +2,7 @@ import ProfileService from '#services/profile_services'
 import { profileUpdateValidator } from '#validators/profile'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
+import db from '@adonisjs/lucid/services/db'
 
 @inject()
 export default class ProfilesController {
@@ -12,13 +13,25 @@ export default class ProfilesController {
     return view.render('pages/profiles/edit', { profile })
   }
 
-  async update({ request, response, auth }: HttpContext) {
+  async update({ request, response, auth, session }: HttpContext) {
     const { fullName, description } = await request.validateUsing(profileUpdateValidator)
 
-    const profile = await this.profileService.find()
-    await profile.merge({ description }).save()
+    const trx = await db.transaction()
 
-    await auth.user!.merge({ fullName }).save()
+    auth.user!.useTransaction(trx)
+
+    try {
+      const profile = await this.profileService.find()
+
+      await auth.user!.merge({ fullName }).save()
+
+      await profile.merge({ description }).save()
+
+      trx.commit()
+    } catch (error) {
+      trx.rollback()
+      session.flash('errorsBag.form', 'Something went wrong!!!')
+    }
 
     return response.redirect().back()
   }
